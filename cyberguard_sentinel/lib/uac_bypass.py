be6fd571b37ef7d6911ab6a035a725751973be3c
CyberDeathSicko <145595167+CyberDeathSicko@users.noreply.github.com>
2023-12-09 07:34:02 -0500

    Update uac_bypass.py

--- cyberguard_sentinel/lib/uac_bypass.py
+++ cyberguard_sentinel/lib/uac_bypass.py
@@ -1,48 +1,36 @@
-# Function for UAC Bypass with GUI
 def uac_bypass_with_gui():
     print("Performing UAC Bypass with GUI...")
-    # Implement UAC Bypass with GUI logic here
-    uac_code = input("Enter your UAC Bypass code for UAC Bypass with GUI: ")
-    # Execute the UAC Bypass code
-    # Example: eval(uac_code)
-
-# Function for UAC Bypass exploit
-def uac_bypass_exploit():
-    print("Performing UAC Bypass exploit...")
-    # Implement UAC Bypass exploit logic here
-    uac_code = input("Enter your UAC Bypass code for UAC Bypass exploit: ")
-    # Execute the UAC Bypass code
-    # Example: eval(uac_code)
-
-# Function for Generic UAC Bypass
-def generic_uac_bypass():
-    print("Performing Generic UAC Bypass...")
-    # Implement Generic UAC Bypass logic here
-    uac_code = input("Enter your UAC Bypass code for Generic UAC Bypass: ")
-    # Execute the UAC Bypass code
-    # Example: eval(uac_code)
-
-# Function to display UAC Bypass options
-def uac_bypass_options():
-    print("Select UAC Bypass option:")
-    print("1. UAC Bypass with GUI")
-    print("2. UAC Bypass exploit")
-    print("3. Generic UAC Bypass")
-    print("0. Return to Main Menu")
+    try:
+        uac_code = input("Enter your UAC Bypass code for UAC Bypass with GUI: ")
+        # Validate and sanitize the input if necessary
+        if not uac_code.isalnum():
+            raise ValueError("Invalid UAC Bypass code format.")
+        
+        # Implement UAC Bypass with GUI logic here
+        # Example: subprocess.run(['command', '/param', uac_code], check=True)
+        print("UAC Bypass executed successfully.")
+    except subprocess.CalledProcessError as e:
+        print("Error executing UAC Bypass:", str(e))
+    except Exception as e:
+        print("Error:", str(e))
 
 # Function to perform UAC Bypass based on the user's choice
 def perform_uac_bypass():
     while True:
         uac_bypass_options()
-        choice = input("Enter your UAC Bypass choice (0-3): ")
+        try:
+            choice = int(input("Enter your UAC Bypass choice (0-3): "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
 
-        if choice == '0':
+        if choice == 0:
             break
-        elif choice == '1':
+        elif choice == 1:
             uac_bypass_with_gui()
-        elif choice == '2':
+        elif choice == 2:
             uac_bypass_exploit()
-        elif choice == '3':
+        elif choice == 3:
             generic_uac_bypass()
         else:
             print("Invalid choice. Please try again.")
