72a21445ab3911311df4e8f8580619094fde3e2d
CyberDeathSicko <145595167+CyberDeathSicko@users.noreply.github.com>
2023-12-08 22:05:17 -0500

    Update uac_bypass.sh

--- cyberguard_sentinel/lib/uac_bypass.sh
+++ cyberguard_sentinel/lib/uac_bypass.sh
@@ -1,57 +1,49 @@
 #!/bin/bash
 
-# UAC Bypass Bash script
+# Function for UAC Bypass with GUI
+function uac_bypass_with_gui() {
+    echo "Performing UAC Bypass with GUI..."
+    # I'm creating it, just need some time for it
+    read -p "Enter your UAC Bypass code for UAC Bypass with GUI: " uac_code
+    eval "$uac_code"
+}
 
-# Array of UAC Bypass options
-uac_options=("Option 1" "Option 2" "Option 3")
-num_options=${#uac_options[@]}
+# Function for UAC Bypass exploit
+function uac_bypass_exploit() {
+    echo "Performing UAC Bypass exploit..."
+    # I'm creating it, just need some time for it
+    read -p "Enter your UAC Bypass code for UAC Bypass exploit: " uac_code
+    eval "$uac_code"
+}
+
+# Function for Generic UAC Bypass
+function generic_uac_bypass() {
+    echo "Performing Generic UAC Bypass..."
+    # I'm creating it, just need some time for it
+    read -p "Enter your UAC Bypass code for Generic UAC Bypass: " uac_code
+    eval "$uac_code"
+}
 
 # Function to display UAC Bypass options
-uac_bypass_menu() {
-    echo "UAC Bypass Section"
-    for ((i=0; i<num_options; i++)); do
-        echo "$(($i+1)). ${uac_options[$i]}"
-    done
+function uac_bypass_options() {
+    echo "Select UAC Bypass option:"
+    echo "1. UAC Bypass with GUI"
+    echo "2. UAC Bypass exploit"
+    echo "3. Generic UAC Bypass"
     echo "0. Return to Main Menu"
 }
 
-# Function for UAC Bypass Option 1
-option_1() {
-    echo "Performing UAC Bypass Option 1..."
-    # Add your UAC Bypass code for Option 1 here
-    # Example: psexec -accepteula -s -i cmd.exe /c your_command_here
-    # Remember to include the necessary UAC bypass techniques specific to your target system
-    read -p "Enter your UAC Bypass code for Option 1: " uac_code
-    eval "$uac_code"
-}
-
-# Function for UAC Bypass Option 2
-option_2() {
-    echo "Performing UAC Bypass Option 2..."
-    # Add your UAC Bypass code for Option 2 here
-    read -p "Enter your UAC Bypass code for Option 2: " uac_code
-    eval "$uac_code"
-}
-
-# Function for UAC Bypass Option 3
-option_3() {
-    echo "Performing UAC Bypass Option 3..."
-    # Add your UAC Bypass code for Option 3 here
-    read -p "Enter your UAC Bypass code for Option 3: " uac_code
-    eval "$uac_code"
-}
-
-# Function to perform UAC Bypass
-perform_uac_bypass() {
+# Function to perform UAC Bypass based on user's choice
+function perform_uac_bypass() {
     while true; do
-        uac_bypass_menu
-        read -p "Enter your choice (0-$num_options): " choice
+        uac_bypass_options
+        read -p "Enter your UAC Bypass choice (0-3): " choice
 
         case $choice in
             0) break;;
-            [1-$num_options])
-                selected_option=$((choice-1))
-                ${uac_options[$selected_option]};;
+            1) uac_bypass_with_gui;;
+            2) uac_bypass_exploit;;
+            3) generic_uac_bypass;;
             *) echo "Invalid choice. Please try again.";;
         esac
     done
